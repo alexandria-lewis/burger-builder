5c19729694bb6ca480ec5f02d14c4ccf67321a2d
Alexandria <alexandriamlewis@me.com>
2019-06-08 13:15:24 -0400

    mod 171: using a logo in our app

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import classes from './Toolbar.css';
 // keep in mind, this import in the end simply imports a couple of strings, the dynamically created css class names which we simply adjusted to be scoped to this component and then, we're assigning the class name which was generated here.
+import Logo from '../../Logo/Logo';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo />
         <nav>
             ...
         </nav>
