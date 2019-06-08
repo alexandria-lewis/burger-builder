99e0ee0df3409bd15834f4e07ee0e1e93e8f0b0d
Alexandria <alexandriamlewis@me.com>
2019-06-08 17:12:16 -0400

    mod 173: creating a responsive sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -8,10 +8,12 @@ created the hoc folder for this
 import classes from './Layout.css';
 //classes can be any word, just used to assign obj params
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Aux>
         <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
