9952d227093bc4a65973e79b7201518b4ef06817
Alexandria <alexandriamlewis@me.com>
2019-06-08 22:45:19 -0400

    mod 176: reusing the backdrop - refactor to close the sideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 /*
@@ -10,14 +10,33 @@ import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+// So in my opinion, it makes more sense to turn the layout component into a class component where we can implement the method so that we can listen to both the sideDrawer closing itself by clicking on the backdrop as well as toolbar opening the sideDrawer by clicking on that toggle button.
 
-export default layout;
+// class based components use capitols
+class Layout extends Component{
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer 
+                    open={this.state.showSideDrawer} 
+                    // we need to pass the open property to the sideDrawer to now and that should be this.state.showSideDrawer because this will hold the information whether the sideDrawer should be opened or not.
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                    {/* props can now be accessed with this.props as you learned, that's the case for all class based components. */}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout;
