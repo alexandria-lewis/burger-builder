a6b6a57e3ce9083fc8b724228c5b07d590b4c251
Alexandria <alexandriamlewis@me.com>
2019-09-10 13:45:38 -0400

    mod 302: basic redux setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);// mod 302 created store
 
 // Now I'll create a new constant which I'll just name app, I'll use parentheses to be able to write multi-line jsx code and then I'll use the browser router opening and closing tag and in between I'll put my app like this.
 const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
 
 // And now I'll use the app const that I just created down there as an argument to pass it down here in react dom render.
