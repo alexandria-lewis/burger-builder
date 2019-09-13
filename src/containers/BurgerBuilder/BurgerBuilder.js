e6ab75b89878d4b768bb9d0ff41cfca55d4047d7
Alexandria <alexandriamlewis@me.com>
2019-09-13 14:07:22 -0400

    mod 306: working on the total price calculation

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,7 +13,6 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
     state = {
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -40,7 +39,7 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el;
             }, 0);
-            this.setState({purchasable: sum > 0});
+            return sum > 0;
     }
 
     purchaseHandler = () => {
@@ -89,7 +88,8 @@ class BurgerBuilder extends Component {
                         ingredientRemoved={this.props.onIngredientRemoved}
                         price={this.props.price}
                         disabled={disabledInfo}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
+                        // here we need to execute this because we want to execute this whenever this gets re-rendered so that we fetch the updated result
                         ordered={this.purchaseHandler}// this method gets executed when we click the order now button
                          />
                 </Aux>
