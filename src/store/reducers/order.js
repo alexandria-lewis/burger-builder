e571fc67c8fe39a7dd3185a713e74d0952f0c11e
Alexandria <alexandriamlewis@me.com>
2019-09-28 18:28:29 -0400

    mod 337: the order reducer

---
+++ src/store/reducers/order.js
@@ -0,0 +1,27 @@
+import * as actionTypes from '../actions/index';
+
+const initialState = {
+    order: [],
+    loading: false
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.PURCHASE_BURGER_SUCCESS:
+            const newOrder = {
+                ...action.orderData,
+                id: action.orderId
+            }
+            return {
+                ...state,
+                loading: false,
+                orders: state.orders.concat(newOrder)
+            }
+        case actionTypes.PURCHASE_BURGER_FAIL:
+            return {}
+        default:
+            return state;
+    }
+}
+
+export default reducer;
